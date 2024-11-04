25a576604d25d3b05c2fac3f3979c68ba02ff757
mikhail <pigolkin.mikhail@yandex.ru>
2024-11-04 16:37:51 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Мое первое окно");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 400);
+        frame.setVisible(true);
+    }
+}
